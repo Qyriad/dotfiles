2cdb53416b18d74f4b3d23072fd3ebce20d0355f
Mikaela Szekely <qyriad@gmail.com>
2019-03-15 09:10:55 -0600

    Add experimental pkglist

---
+++ pkglist.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+pkglist=(moreutils zsync android-udev atool ccls downgrade ffmpeg ranger neovim fzf ripgrep fd exa grc wget gpaste-client neovim-remote pydf insect htop tmux zplug)
+
+#echo $pkglist
+
+for i in $pkglist; do
+	echo $i
+done
