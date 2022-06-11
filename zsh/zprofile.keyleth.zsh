9fb6c111b301195f96bd3f62a2b908c4c8934acf
Mikaela Szekely <mikaela.szekely@qyriad.me>
2022-06-10 20:20:18 -0600

    zsh: cleanup Keyleth zprofile and add GNU Make to PATH

--- zsh/zprofile.keyleth.zsh
+++ zsh/zprofile.keyleth.zsh
@@ -2,7 +2,10 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 export HOMEBREW_DISPLAY_INSTALL_TIMES=1
 export HOMEBREW_KEEP_OUTPUT=1
 PATH="/Users/qyriad/.local/bin:$PATH"
-PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:/opt/homebrew/opt/python/libexec/bin:/usr/local/MacGPG2/bin:$PATH:/opt/homebrew-rosetta/bin:/opt/homebrew/opt/llvm/bin"
+COREUTILSBIN="/opt/homebrew/opt/coreutils/libexec/gnubin"
+MAKEBIN="/opt/homebrew/opt/make/libexec/gnubin"
+PYTHONBIN="/opt/homebrew/opt/python@3.10/libexec/bin"
+PATH="$COREUTILSBIN:$MAKEBIN:$PYTHONBIN:/usr/local/MacGPG2/bin:$PATH:/opt/homebrew-rosetta/bin:/opt/homebrew/opt/llvm/bin"
 PATH="/Users/qyriad/.local/share/cargo/bin:$PATH"
 export XDG_CACHE_HOME="/Users/qyriad/.cache"
 export XDG_CONFIG_HOME="/Users/qyriad/.config"
