fb004c150b9a5047eb96f39e0ae6ddad8cbf4fe3
Mikaela Szekely <mikaela.szekely@qyriad.me>
2022-07-08 14:30:02 -0600

    zprofile.macos: set BOOTSNAP, NO_AUTO_UPDATe, and UPDATE_REPORT_ALL_FORMULAE

--- zsh/zprofile.macos.zsh
+++ zsh/zprofile.macos.zsh
@@ -1,6 +1,9 @@
 # eval $(brew shellenv) should be called before including this script.
 
+export HOMEBREW_BOOTSNAP=1
+export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_DISPLAY_INSTALL_TIMES=1
+export HOMEBREW_UPDATE_REPORT_ALL_FORMULAE=1
 export HOMEBREW_KEEP_OUTPUT=1
 
 PATH="$HOME/.local/bin:$PATH"
