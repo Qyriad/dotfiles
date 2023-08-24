3dee3d66a2676bfafad6f452b13cf06bf74bd509
Qyriad <qyriad@qyriad.me>
2023-08-24 17:27:45 -0600

    oops I forgot to commit udev-rules/default.nix

---
+++ nixos/udev-rules/default.nix
@@ -0,0 +1,14 @@
+{ pkgs }:
+pkgs.stdenv.mkDerviation {
+  pname = "qyriad-udev-rules";
+  meta.description = "Qyriad personal udev rules";
+  version = "0.1.0";
+  dontBuild = true;
+  dontConfigure = true;
+  src = ./.;
+
+  installPhase = ''
+    mkdir -p $out/lib/udev/rules.d
+    cp -v $src/*.rules $out/lib/udev/rules.d
+  '';
+}
