1668eced09cb12b47e5ed70066316b1632f7f799
Qyriad <qyriad@qyriad.me>
2023-08-30 21:41:51 -0600

    nixos: switch to unstable
    Rolling release go brrrr

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 
 {
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/23.05";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		flake-utils.url = "github:numtide/flake-utils";
 		nixseparatedebuginfod.url = "github:symphorien/nixseparatedebuginfod";
 	};
