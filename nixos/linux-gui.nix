5d9a3ad7468d4cf065a27d40ae0bd91f131ea04b
Qyriad <qyriad@qyriad.me>
2023-09-16 02:38:13 -0600

    nixos: enable fcitx5 with mozc for Japanese input

--- nixos/linux-gui.nix
+++ nixos/linux-gui.nix
@@ -66,6 +66,15 @@
 
 	programs.gnupg.agent.pinentryFlavor = "qt";
 
+	# Input method stuff.
+	i18n.inputMethod = {
+		enabled = "fcitx5";
+
+		fcitx5.addons = with pkgs; [
+			fcitx5-mozc
+		];
+	};
+
 	# Enable udisks2.
 	services.udisks2.enable = true;
 
