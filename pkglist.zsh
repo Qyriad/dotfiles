fcd341be6ef59cf7370232e159c5874c2d2ef820
Qyriad <qyriad@qyriad.me>
2023-10-30 20:08:36 -0600

    unused: remove pkglist.zsh

--- pkglist.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-pkglist=(moreutils zsync android-udev atool ccls downgrade ffmpeg ranger neovim fzf ripgrep fd exa grc wget gpaste-client neovim-remote pydf insect htop tmux zplug)
-
-#echo $pkglist
-
-for i in $pkglist; do
-	echo $i
-done
