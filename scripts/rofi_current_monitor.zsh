ef5d78531a4d66c77bc0b89b563c4f980b1ef3b1
Qyriad <qyriad@qyriad.me>
2023-10-30 20:10:08 -0600

    unused: remove ancient files in ./scripts

--- scripts/rofi_current_monitor.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-# I hate this
-
-OUTPUT_POS=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused).rect | "\(.width)x\(.height)\\+\(.x)\\+\(.y)"')
-XWAYLAND_OUTPUT=$(xrandr | rg -oP "XWAYLAND[0-9](?= connected $OUTPUT_POS)")
-ROFI_ID=$(rofi --help | rg "$XWAYLAND_OUTPUT" -B 1 | head -1 | rg -o '\d+')
-print -n $ROFI_ID
