992c211fae21a75f99c05d15a7cdb86f2bf5f611
Qyriad <qyriad@qyriad.me>
2024-01-04 15:05:12 -0700

    prev: fix indentation lol

--- default.nix
+++ default.nix
@@ -1,12 +1,13 @@
+# vim: shiftwidth=4 tabstop=4 noexpandtab
 let
-  flakeref = builtins.getFlake (toString ./.);
-  mkForSystem = self: { system ? builtins.currentSystem }:
-    self // {
-      packages = self.packages.${system};
-      legacyPackages = self.legacyPackages.${system};
-    }
-  ;
+	flakeref = builtins.getFlake (toString ./.);
+	mkForSystem = self: { system ? builtins.currentSystem }:
+		self // {
+			packages = self.packages.${system};
+			legacyPackages = self.legacyPackages.${system};
+		}
+	;
 in
-  flakeref // {
-    __functor = mkForSystem;
-  }
+	flakeref // {
+		__functor = mkForSystem;
+	}
