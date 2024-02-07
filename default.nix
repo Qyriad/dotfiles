ec760f9907aa55ba62cfe62ee3e7f4adb44f863a
Qyriad <qyriad@qyriad.me>
2024-02-07 14:25:22 -0700

    nixos: towards a more useful default.nix

--- default.nix
+++ default.nix
@@ -1,13 +1,46 @@
 # vim: shiftwidth=4 tabstop=4 noexpandtab
+{
+	pkgs ? import <nixpkgs> { },
+}:
 let
-	flakeref = builtins.getFlake (toString ./.);
-	mkForSystem = self: { system ? builtins.currentSystem }:
-		self // {
-			packages = self.packages.${system};
-			legacyPackages = self.legacyPackages.${system};
-		}
+	inherit (pkgs) lib;
+
+	lockFile = builtins.fromJSON (builtins.readFile ./flake.lock);
+	# lockFile but the parts that matter.
+	lockFile' = lib.mapAttrs (k: v: v.locked) lockFile.nodes;
+
+	getLockedUrl = locked:
+		"https://github.com/archive/${locked.owner}/${locked.repo}/${locked.rev}.tar.gz"
 	;
-in
-	flakeref // {
-		__functor = mkForSystem;
-	}
+
+	mkEachFlakeInput = lib.mapAttrs (name: locked: let
+		fetched = fetchTarball {
+			url = getLockedUrl locked;
+			sha256 = locked.narHash;
+		};
+	in
+		fetched
+	);
+
+	flakeInputs = mkEachFlakeInput lockFile';
+
+	qyriad-nur = import flakeInputs.qyriad-nur { };
+
+in {
+	nix-helpers = pkgs.callPackage ./nixos/pkgs/nix-helpers.nix { };
+	xonsh = pkgs.callPackage ./nixos/pkgs/xonsh {
+		inherit (qyriad-nur) python-pipe xontrib-abbrevs xonsh-direnv;
+	};
+}
+
+#	flakeref = builtins.getFlake (toString ./.);
+#	mkForSystem = self: { system ? builtins.currentSystem }:
+#		self // {
+#			packages = self.packages.${system};
+#			legacyPackages = self.legacyPackages.${system};
+#		}
+#	;
+#in
+#	flakeref // {
+#		__functor = mkForSystem;
+#	}
