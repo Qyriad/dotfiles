c1e28357e8f087a0209a2a3e3db649d2c18669fb
Qyriad <qyriad@qyriad.me>
2024-04-04 21:14:35 -0600

    nixos: fix template input URLs

--- nixos/templates/base/flake.nix
+++ nixos/templates/base/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs";
-    flake-utils.url = "flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
