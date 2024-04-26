ba8ad9f6a0314c4e8efb32c420eb6015727f09a8
Qyriad <qyriad@qyriad.me>
2024-04-26 06:48:49 -0600

    nvim: guard the ts-context disable on ts-context existing

--- nvim/after/ftplugin/nix.lua
+++ nvim/after/ftplugin/nix.lua
@@ -1,3 +1,5 @@
-require("treesitter-context").setup {
-	enable = false,
-}
+if #vim.fn.globpath(vim.o.runtimepath, 'lua/treesitter-context') > 0 then
+	require("treesitter-context").setup {
+		enable = false,
+	}
+end
