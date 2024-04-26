6f918415eede2494d41f9895920192f5ec7fa0d7
Qyriad <qyriad@qyriad.me>
2024-04-26 07:15:31 -0600

    nixos: remove unneeded nix-helpers

--- nixos/pkgs/nix-helpers.nix
+++ nixos/pkgs/nix-helpers.nix
@@ -12,18 +12,6 @@ let
     exec $cmd
   '';
 
-  nixpkgs-cmd = pkgs.writeShellScriptBin "nixpkgs" ''
-    echo $(nix eval --impure --expr "<nixpkgs>")
-  '';
-
-  nix-print-cmd = pkgs.writeShellScriptBin "nix-print" ''
-    for _i in $(seq $#); do
-      niz eval --raw 'qyriad#pkgs' --apply "pkgs: pkgs.lib.generators.toPretty { allowPrettyValues = true; } ($1)"
-      echo
-      shift
-    done
-  '';
-
   highlight-nix = pkgs.writeShellScriptBin "hlnix" ''
     exec bat -l nix --no-pager --style=plain $@
   '';
@@ -31,11 +19,9 @@ let
 in
   pkgs.symlinkJoin {
     name = "nix-helpers";
-    inherit rebuild-cmd nixpkgs-cmd;
+    inherit rebuild-cmd;
     paths = [
       rebuild-cmd
-      nixpkgs-cmd
-      nix-print-cmd
       highlight-nix
     ];
   }
