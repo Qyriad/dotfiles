ac354e0610352a8f628b645051aaa701e977e847
Qyriad <qyriad@qyriad.me>
2024-05-11 16:39:20 -0600

    nixos: add lix cache

--- nixos/common.nix
+++ nixos/common.nix
@@ -8,6 +8,14 @@
 		settings = {
 			experimental-features = [ "nix-command" "flakes" ];
 
+			extra-substituters = [
+				"https://cache.lix.systems"
+			];
+
+			trusted-public-keys = [
+				"cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+			];
+
 			trusted-users = [
 				"root"
 			];
