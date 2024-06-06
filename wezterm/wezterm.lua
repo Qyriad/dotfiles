4bb7966e65ef601b626a9c82f2b0a8bb9a698a4c
Qyriad <qyriad@qyriad.me>
2024-06-06 09:10:18 -0600

    wezterm: use local domain instead of multiplexer
    We'll still use tmux for multiplexing

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -62,6 +62,7 @@ local config = wezterm.config_builder()
 
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
 
 function format_tab_title(tab, tabs, panes, config, hover, max_width)
 	local title = utils.tab_title(tab)
@@ -91,7 +92,7 @@ wezterm.on("format-tab-title", format_tab_title)
 config.default_prog = { "zsh", "--login", "-c", "exec xonsh" }
 
 config.font = wezterm.font_with_fallback(font_list)
-config.font_size = 13
+config.font_size = 12
 config.font_rules = {
 	{
 		intensity = "Bold",
@@ -109,10 +110,12 @@ config.bold_brightens_ansi_colors = "BrightAndBold"
 
 
 -- Multiplexing.
-config.unix_domains = {
-	{ name = "unix" },
-}
-config.default_gui_startup_args = { "connect", "unix" }
+--config.unix_domains = {
+--	{ name = "unix" },
+--}
+--config.default_gui_startup_args = { "connect", "unix" }
+--config.default_gui_startup_args = { "connect", "SSHMUX:localhost" }
+config.default_domain = "local"
 
 ssh_domain_overrides = {
 	["SSHMUX:yuki"] = {
@@ -135,74 +138,79 @@ for _, domain in ipairs(config.ssh_domains) do
 end
 
 -- Main keybindings. Set up to act kind of like our tmux config.
-config.leader = {
-	key = "`"
-}
+--config.leader = {
+--	key = "`"
+--}
 
 config.keys = {
-	-- Equivalent to `bind-key -T prefix "`" send-prefix`
-	bind_leader("`", wezterm.action.SendString("`")),
-
-	-- Equivalent to `bind-key -T prefix '"' split-window -v`
-	-- domain = "CurrentPaneDomain" is the default.
-	bind_leader('"', wezterm.action.SplitVertical { }, "SHIFT"),
-
-	-- Equivalent to `bind-key -T prefix "%" split-window -h`
-	bind_leader("%", wezterm.action.SplitHorizontal { }, "SHIFT"),
-
-	-- Equivalent to `bind-key -T prefix "c" new-window
-	bind_leader("c", wezterm.action.SpawnTab("CurrentPaneDomain")),
-
-	-- Equivalent to `bind-key -T prefix "n" next-window
-	bind_leader("n", wezterm.action.ActivateTabRelative(1)),
-
-	-- Equivalent to `bind-key -T prefix "p" previous-window
-	bind_leader("p", wezterm.action.ActivateTabRelative(-1)),
-
-	-- Equivalent to bind-key -T prefix "x" confirm-before -p "kill-pane #P? (y-n) kill-pane"
-	bind_leader("x", wezterm.action.CloseCurrentPane { confirm = true }),
-
-	-- Equivalent to bind-key -T prefix "h" select-pane -L
-	bind_leader("h", wezterm.action.ActivatePaneDirection("Left")),
-
-	-- Equivalent to bind-key -T prefix "j" select-pane -D
-	bind_leader("j", wezterm.action.ActivatePaneDirection("Down")),
-
-	-- Equivalent to bind-key -T prefix "k" select-pane -U
-	bind_leader("k", wezterm.action.ActivatePaneDirection("Up")),
-
-	-- Equivalent to bind-key -T prefix "l" select-pane -L
-	bind_leader("l", wezterm.action.ActivatePaneDirection("Right")),
-
-	-- Equivalent to `bind-key -T prefix "-" select-window -l`
-	bind_leader("-", wezterm.action.ActivateLastTab),
-
-	-- Equivalent to `bind-key -T prefix "z" resize-pane -Z`
-	bind_leader("z", wezterm.action.TogglePaneZoomState),
-
-	-- Equivalent to `bind-key -T prefix "[" copy-mode`
-	bind_leader("[", wezterm.action.ActivateCopyMode),
-
-	bind_leader("d", wezterm.action.DetachDomain "CurrentPaneDomain"),
-
-	bind_leader(":", wezterm.action.ActivateCommandPalette),
-	bind_leader(";", wezterm.action.ActivateCommandPalette),
-
-	bind_leader("1", wezterm.action.ActivateTab(0)),
-	bind_leader("2", wezterm.action.ActivateTab(1)),
-	bind_leader("3", wezterm.action.ActivateTab(2)),
-	bind_leader("4", wezterm.action.ActivateTab(3)),
-	bind_leader("5", wezterm.action.ActivateTab(4)),
-	bind_leader("6", wezterm.action.ActivateTab(5)),
-	bind_leader("7", wezterm.action.ActivateTab(6)),
-	bind_leader("8", wezterm.action.ActivateTab(7)),
-	bind_leader("9", wezterm.action.ActivateTab(8)),
-	bind_leader("0", wezterm.action.ActivateTab(0)),
+	---- Equivalent to `bind-key -T prefix "`" send-prefix`
+	--bind_leader("`", wezterm.action.SendString("`")),
+	--
+	---- Equivalent to `bind-key -T prefix '"' split-window -v`
+	---- domain = "CurrentPaneDomain" is the default.
+	--bind_leader('"', wezterm.action.SplitVertical { }, "SHIFT"),
+	--
+	---- Equivalent to `bind-key -T prefix "%" split-window -h`
+	--bind_leader("%", wezterm.action.SplitHorizontal { }, "SHIFT"),
+	--
+	---- Equivalent to `bind-key -T prefix "c" new-window
+	--bind_leader("c", wezterm.action.SpawnTab("CurrentPaneDomain")),
+	--
+	---- Equivalent to `bind-key -T prefix "n" next-window
+	--bind_leader("n", wezterm.action.ActivateTabRelative(1)),
+	--
+	---- Equivalent to `bind-key -T prefix "p" previous-window
+	--bind_leader("p", wezterm.action.ActivateTabRelative(-1)),
+	--
+	---- Equivalent to bind-key -T prefix "x" confirm-before -p "kill-pane #P? (y-n) kill-pane"
+	--bind_leader("x", wezterm.action.CloseCurrentPane { confirm = true }),
+	--
+	---- Equivalent to bind-key -T prefix "h" select-pane -L
+	--bind_leader("h", wezterm.action.ActivatePaneDirection("Left")),
+	--
+	---- Equivalent to bind-key -T prefix "j" select-pane -D
+	--bind_leader("j", wezterm.action.ActivatePaneDirection("Down")),
+	--
+	---- Equivalent to bind-key -T prefix "k" select-pane -U
+	--bind_leader("k", wezterm.action.ActivatePaneDirection("Up")),
+	--
+	---- Equivalent to bind-key -T prefix "l" select-pane -L
+	--bind_leader("l", wezterm.action.ActivatePaneDirection("Right")),
+	--
+	---- Equivalent to `bind-key -T prefix "-" select-window -l`
+	--bind_leader("-", wezterm.action.ActivateLastTab),
+	--
+	---- Equivalent to `bind-key -T prefix "z" resize-pane -Z`
+	--bind_leader("z", wezterm.action.TogglePaneZoomState),
+	--
+	---- Equivalent to `bind-key -T prefix "[" copy-mode`
+	--bind_leader("[", wezterm.action.ActivateCopyMode),
+	--
+	--bind_leader("d", wezterm.action.DetachDomain "CurrentPaneDomain"),
+	--
+	--bind_leader(":", wezterm.action.ActivateCommandPalette),
+	--bind_leader(";", wezterm.action.ActivateCommandPalette),
+	--
+	--bind_leader("1", wezterm.action.ActivateTab(0)),
+	--bind_leader("2", wezterm.action.ActivateTab(1)),
+	--bind_leader("3", wezterm.action.ActivateTab(2)),
+	--bind_leader("4", wezterm.action.ActivateTab(3)),
+	--bind_leader("5", wezterm.action.ActivateTab(4)),
+	--bind_leader("6", wezterm.action.ActivateTab(5)),
+	--bind_leader("7", wezterm.action.ActivateTab(6)),
+	--bind_leader("8", wezterm.action.ActivateTab(7)),
+	--bind_leader("9", wezterm.action.ActivateTab(8)),
+	--bind_leader("0", wezterm.action.ActivateTab(0)),
 
 	bind_os("c", wezterm.action.CopyTo("Clipboard"), { ctrl_shift = true }),
 	bind_os("v", wezterm.action.PasteFrom("Clipboard"), { ctrl_shift = true }),
 
 	bind_os("-", wezterm.action.DecreaseFontSize),
+}
+
+config.key_tables = { }
+
+config.key_tables.copy_mode = {
 }
 
 config.mouse_bindings = {
