c5d36593eb5a1c4864d7dd51885e9ddf56857e0c
Qyriad <qyriad@qyriad.me>
2024-07-04 22:34:08 -0600

    nixos: fix xonsh (again)

--- nixos/pkgs/xonsh/default.nix
+++ nixos/pkgs/xonsh/default.nix
@@ -9,36 +9,21 @@
 
 let
 
-  xonsh-unwrapped = let
-    xonsh-unwrapped-nixpkgs = pkgs.xonsh-unwrapped;
-  in xonsh-unwrapped-nixpkgs.overridePythonAttrs (prev: {
-    src = xonsh-source;
-
-    propagatedBuildInputs = prev.propagatedBuildInputs or [ ] ++ [
-      pkgs.python3Packages.requests
+  xonshEnv = pkgs.xonsh.wrapper.override {
+    extraPackages = py: [
+      py.unidecode
+      py.psutil
+      py.requests
+      py.pygments
+      py.jsondiff
+      py.setproctitle
+      py.httpx
+      xonsh-direnv
+      xontrib-abbrevs
+      python-pipe
     ];
 
-    # I *assume* the failing tests are from the sandbox.
-    # Eh, it's just our shell, what could go wrong?
-    doCheck = false;
-  });
-
-  pythonEnv = lib.setName "python-xonsh-env" (pkgs.python3.withPackages (py: [
-    (py.toPythonModule xonsh-unwrapped)
-    py.unidecode
-    py.psutil
-    py.requests
-    py.pygments
-    py.jsondiff
-    py.setproctitle
-    py.httpx
-    xonsh-direnv
-    xontrib-abbrevs
-    python-pipe
-  ])) // {
-    # I honestly have no idea if the precedence makes this happen before or after
-    # the lib.setName, but it doesn't matter lol
-    inherit (xonsh-unwrapped) meta passthru;
+    xonsh = pkgs.xonsh.overridePythonAttrs { dontWrapPythonPrograms = true; };
   };
 
-in pythonEnv
+in xonshEnv
