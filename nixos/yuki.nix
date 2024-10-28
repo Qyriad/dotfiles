f021129479c723cb85da81453f9bc485ab937f99
Qyriad <qyriad@qyriad.me>
2024-10-28 14:08:21 -0600

    nixos: yuki: restore samba settings

--- nixos/yuki.nix
+++ nixos/yuki.nix
@@ -106,19 +106,16 @@
 			};
 		};
 		nsswins = true;
-		#extraConfig = ''
-		#    workgroup = WORKGROUP
-		#	server string = yuki
-		#	netbios name = yuki
-		#	security = user
-		#	#use sendfile = yes
-		#	#max protocol = smb2
-		#	# note: localhost is the ipv6 localhost ::1
-		#	hosts allow = 192.168.50. 127.0.0.1 localhost
-		#	hosts deny = 0.0.0.0/0
-		#	guest account = nobody
-		#	map to guest = bad user
-		#'';
+		settings.global = {
+			workgroup = "WORKGROUP";
+			"server string" = "yuki";
+			"netbios name" = "yuki";
+			"security" = "user";
+			"hosts allow" = "192.168.50. 127.0.0.1 localhost";
+			"hosts deny" = "0.0.0.0/0";
+			"guest account" = "nobody";
+			"map to guest" = "bad user";
+		};
 	};
 
 	nix.distributedBuilds = true;
