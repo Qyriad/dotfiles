3987aa171cbbcff6ce828e89ff696ac9dec3e5d0
Qyriad <qyriad@qyriad.me>
2024-11-27 15:07:18 -0700

    nixos: add nixpkgs-master, nixpkgs-unstable, and system to repl-overlay

--- nix/repl-overlay.nix
+++ nix/repl-overlay.nix
@@ -4,6 +4,8 @@ rec {
   inherit (builtins) attrValues attrNames getFlake typeOf;
   qyriad = getFlake "qyriad";
   nixpkgs = getFlake "nixpkgs";
+  nixpkgs-master = getFlake "github:NixOS/nixpkgs/master";
+  nixpkgs-unstable = getFlake "github:NixOS/nixpkgs/nixpkgs-unstable";
   pkgs = import nixpkgs {
     system = info.currentSystem;
     overlays = attrValues qyriad.overlays;
@@ -14,4 +16,5 @@ rec {
   #local = import ./. { };
   local = import (builtins.getEnv "PWD") { };
   currentSystem = info.currentSystem;
+  system = info.currentSystem;
 }
