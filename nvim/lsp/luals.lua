3571a8afe71fa3ff68f68660af39f2133d90f075
Qyriad <qyriad@qyriad.me>
2025-04-07 17:38:26 +0200

    nvim: lsp: make luals work for Neovim lua files

--- nvim/lsp/luals.lua
+++ nvim/lsp/luals.lua
@@ -1,5 +1,28 @@
 -- Note: for lspconfig the module name is `lua_ls`.
+local lua_runtime_paths = vim.iter(vim.api.nvim_get_runtime_file("lua/", true))
+	:map(function(item)
+		-- Remove the trailing slash.
+		return item:sub(1, #item - 1)
+	end)
+	:totable()
+
 vim.lsp.config('luals', {
 	filetypes = { 'lua' },
-	cmd = { 'lua-language-server' }
+	cmd = { 'lua-language-server' },
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+				path = { "?.lua", "?/init.lua" },
+				pathStrict = false, -- FIXME: maybe?
+			},
+			workspace = {
+				checkThirdParty = false,
+				library = lua_runtime_paths,
+			},
+			diagnostics = {
+				globals = { 'vim' },
+			},
+		},
+	},
 })
