89ebc4e174fb1fa8071996add5c60873c1a85132
Qyriad <qyriad@qyriad.me>
2025-04-07 17:08:50 +0200

    nvim: lsp: pass along rust-analyzer config in initializationOptions

--- nvim/lsp/rust-analyzer.lua
+++ nvim/lsp/rust-analyzer.lua
@@ -16,4 +16,11 @@ vim.lsp.config('rust-analyzer', {
 			lens = { references = { method = { enable = false } } },
 		},
 	},
+	before_init = function(init_params, config)
+		if config.settings and config.settings['rust-analyzer'] then
+			init_params = vim.tbl_deep_extend('force', init_params, {
+				initializationOptions = config.settings['rust-analyzer'],
+			})
+		end
+	end,
 })
