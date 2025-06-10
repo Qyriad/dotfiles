d7ada32d42fe2a040ff708df3b1c9a9ec8ac48c2
Qyriad <qyriad@qyriad.me>
2025-06-10 11:32:42 +0200

    nixos: cargo-info has been fixed

--- nixos/make-overlay.nix
+++ nixos/make-overlay.nix
@@ -36,23 +36,6 @@
 		qyriad = scope;
 		inherit (final.qyriad) qlib;
 
-		# Nice one, T-libs-api.
-		# https://github.com/rust-lang/rust/issues/127343
-		cargo-info = let
-			src = final.fetchFromGitHub {
-				owner = "Qyriad";
-				repo = "cargo-info";
-				rev = "refs/heads/main";
-				hash = "sha256-kntuacWov8oVoUOVP3M3LhqaNsSB4OY9Jtav9886r+M=";
-			};
-		in prev.cargo-info.overrideAttrs {
-			inherit src;
-			cargoDeps = final.rustPlatform.fetchCargoTarball {
-				inherit src;
-				hash = "sha256-+BnPLSzIJbS7TIYvATiGFliAEDmScw4bPcj8ZC6RKqM=";
-			};
-		};
-
 		# Nil HEAD has support for pipe operator.
 		nil = prev.nil.overrideAttrs {
 			src = nil-source;
