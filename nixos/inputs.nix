83761ba652b4acb15924f9a515b8b88d82f3bef9
Qyriad <qyriad@qyriad.me>
2025-07-10 14:58:24 +0200

    nixos: add inputs.nix (to-be-used)

---
+++ nixos/inputs.nix
@@ -0,0 +1,43 @@
+/** Provide fetched flake inputs for any inputs that *aren't* provided via the
+ * `explicitInputs` attrset argument.
+ *
+ * This is for making our Nix files agnostic across flake-vs-non-flake.
+ */
+let
+  # We must make do without `lib` here.
+  # Builtins and hand-rolled only.
+  inherit (builtins)
+    parseFlakeRef
+    fetchTree
+    mapAttrs
+    removeAttrs
+    attrNames
+    concatStringsSep
+  ;
+
+  parseInput = attrs: if attrs ? url then
+    parseFlakeRef attrs.url
+  else removeAttrs attrs [
+    "flake" # flake = false
+    "inputs" # inputs.foo.follows
+  ];
+
+  fetchInput = spec: parseInput spec |> fetchTree;
+
+  # Of the shape:
+  # { nixpkgs = { url = "…"; }; lix-module = { inputs = { … }; url = "…"}; … }
+  flakeInputs = (import ../flake.nix).inputs;
+
+  fetchedInputs = mapAttrs (_: spec: fetchInput spec) flakeInputs;
+
+  getter = { ... } @ explicitInputs: let
+    validNames = attrNames flakeInputs;
+    givenButInvalidNames = attrNames (removeAttrs explicitInputs validNames);
+    errorNames = concatStringsSep ", " givenButInvalidNames;
+  in if givenButInvalidNames != [ ] then
+    throw "attempted to override flake inputs that don't exist: \"${errorNames}\""
+  else
+    assert givenButInvalidNames == [ ];
+    fetchedInputs // explicitInputs;
+
+in getter
