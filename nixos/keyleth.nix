a552c5c34c0561e8331b749862fc2a0d981f79f2
Qyriad <qyriad@qyriad.me>
2025-07-22 19:34:01 +0200

    nixos: keyleth: fix indentation

--- nixos/keyleth.nix
+++ nixos/keyleth.nix
@@ -1,30 +1,67 @@
-{ pkgs, ... }:
+# vim: shiftwidth=4 tabstop=4 noexpandtab
+{ pkgs, lib, ... }:
 
 {
-  system.stateVersion = 5;
+	imports = [
+		./common.nix
+	];
 
-  nixpkgs.config.allowUnfree = true;
-  services.nix-daemon.enable = true;
+	system.stateVersion = 5;
 
-  nix = {
-    settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-      trusted-users = [ "qyriad" ];
-      repl-overlays = [ ../nix/repl-overlay.nix ];
-    };
+	nixpkgs.config.allowUnfree = true;
+	ids.gids.nixbld = 30000;
 
-    nixPath = [ "nixpkgs=flake:nixpkgs" ];
-  };
+	nix = {
+		settings = {
+			experimental-features = [
+				"nix-command"
+				"flakes"
+				"pipe-operator"
+				"no-url-literals"
+				#"lix-custom-sub-commands"
+				#"impure-derivations"
+				"cgroups"
+				"auto-allocate-uids"
+			];
+			trusted-users = [ "qyriad" ];
+			repl-overlays = [ ../nix/repl-overlay.nix ];
+		};
 
-  environment.systemPackages = with pkgs; [
-    qyriad.xil
-    qyriad.niz
-    qyriad.git-point
-    nixpkgs-review
-    nixfmt-rfc-style
-    nil
-    nix-output-monitor
-    nix-index
-    rustup
-  ];
+		nixPath = [ "nixpkgs=flake:nixpkgs" ];
+	};
+
+	system.primaryUser = "qyriad";
+	system.defaults.CustomUserPreferences = {
+		# Yes really...
+		${lib.escapeShellArg "Apple Global Domain"} = {
+			"AppleMenuBarVisibleInFullscreen" = "1";
+		};
+	};
+
+	environment.systemPackages = with pkgs; [
+		#qyriad.xil
+		qyriad.niz
+		qyriad.git-point
+		nixpkgs-review
+		nixfmt-rfc-style
+		nil
+		nix-output-monitor
+		nix-index
+		rustup
+		repgrep
+		qyriad.xonsh
+		#git-spice
+		git-sizer
+		ast-grep
+		#havn
+		rink
+		harlequin
+		ipatool
+		jtbl
+		imgp
+		pipet
+		sacad
+		mousecape
+		sloth-app
+	];
 }
