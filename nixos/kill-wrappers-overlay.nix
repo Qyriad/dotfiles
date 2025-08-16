69aac8e3481e5d57905be833201da6a26ca3875e
Qyriad <qyriad@qyriad.me>
2025-08-16 11:49:08 +0200

    nixos: don't wrap wl-clipboard just to put xdg-mime in its PATH

---
+++ nixos/kill-wrappers-overlay.nix
@@ -0,0 +1,30 @@
+# vim: tabstop=4 shiftwidth=0 noexpandtab
+final: prev: let
+	inherit (final) lib;
+in {
+	colmena = prev.colmena.overrideAttrs (prev: {
+		# We don't need to wrap colmena *just* to put `nix` in its PATH...
+		preFixup = lib.trim ''
+			if [[ -f "$out/bin/.colmena-wrapped" ]]; then
+				mv -vf "$out/bin/.colmena-wrapped" "$out/bin/colmena"
+			fi
+		'';
+	});
+
+	wl-clipboard = prev.wl-clipboard.overrideAttrs (prev: {
+		# Disable nixpkgs' hook, which normally wraps wl-copy to put
+		# xdg-mime in its PATH.
+		postInstall = null;
+
+		# Instead, we'll make xdg-utils available at build-time...
+		buildInputs = prev.buildInputs ++ [ final.xdg-utils ];
+
+		# and patch wl-copy to use an absolute path instead.
+		xdgMime = lib.getExe' final.xdg-utils "xdg-mime";
+
+		preConfigure = lib.trim ''
+			substituteInPlace src/util/files.c \
+				--replace-fail 'execlp("xdg-mime"' "execlp(\"$xdgMime\""
+		'';
+	});
+}
