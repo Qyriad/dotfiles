cd6d75d46f2348fa529a640a95a45ca064585d53
Qyriad <qyriad@qyriad.me>
2025-08-25 12:33:21 +0200

    nixos: completely re-do xonsh. again.

--- nixos/pkgs/xonsh/default.nix
+++ nixos/pkgs/xonsh/default.nix
@@ -1,50 +1,92 @@
 {
-  pkgs,
   lib,
   xonsh-source,
+  stdlib,
+  python3Packages,
   python-pipe,
   xontrib-abbrevs,
   xonsh-direnv,
+  # FIXME: requires a patch to sitecustomize.py
+  gobject-introspection,
+  gtk3,
+  gtk4,
+  glib,
+  graphene,
+
   # Setting `programs.xonsh.package` to this causes NixOS to call
   # `.override { extraPackages = }` on us.
   extraPackages ? lib.const [ ],
-}:
+  __checks ? extraPackages python3Packages == [ ],
+}: lib.callWith' python3Packages ({
+  unidecode,
+  psutil,
+  requests,
+  jsondiff,
+  setproctitle,
+  httpx,
+  tqdm,
+  pip,
+  pydbus,
+  jeepney,
+  pygobject3,
+  ds-store,
+  xmltodict,
+  python-box,
+  matplotlib,
+  pillow,
+  pytesseract,
+  beautifulsoup4,
+  python-fontconfig,
+  keyring,
+}: stdlib.mkSimpleEnv (self: assert __checks; {
+  extraAttrs.xonsh = python3Packages.xonsh.overridePythonAttrs {
+    src = xonsh-source;
+  };
 
-let
-
-  # This seems to be what makes it all work the best, for now.
-  xonshEnv = pkgs.python3.withPackages (py: [
-    pkgs.xonsh-unwrapped
-    py.ply
-    py.prompt-toolkit
-    py.pygments
-
-    py.unidecode
-    py.psutil
-    py.requests
-    py.jsondiff
-    py.setproctitle
-    py.httpx
-    py.tqdm
-    py.pip
-    py.pydbus
-    py.jeepney
-    py.pygobject3
-    py.jeepney
-    py.ds-store
-    py.xmltodict
-    py.python-box
-    py.matplotlib
-    py.pillow
-    py.pytesseract
-    py.beautifulsoup4
-    py.python-fontconfig
-    py.keyring
-    pkgs.gobject-introspection
-    pkgs.gtk3
-    pkgs.glib
-    xonsh-direnv
-    xontrib-abbrevs
+  layers = [
+    self.xonsh
     python-pipe
-  ] ++ (extraPackages py));
-in xonshEnv
+    xontrib-abbrevs
+    xonsh-direnv
+
+    unidecode
+    psutil
+    requests
+    jsondiff
+    setproctitle
+    httpx
+    tqdm
+    pip
+    pydbus
+    jeepney
+    pygobject3
+    ds-store
+    xmltodict
+    python-box
+    matplotlib
+    pillow
+    pytesseract
+    beautifulsoup4
+    python-fontconfig
+    keyring
+    pygobject3
+    gtk4
+    #glib
+    #graphene
+    #gobject-introspection
+  ] ++ lib.concatLists [
+    self.xonsh.propagatedBuildInputs
+  ];
+
+  extraAttrs.pythonShebang = "#!" + (builtins.placeholder "out") + "/bin/python3";
+
+  # FIXME: patch all Python things in /bin
+  postInstall = ''
+    sed --in-place -e "1c$pythonShebang" "$out/bin/xonsh"
+    sed --in-place -e "1c$pythonShebang" "$out/bin/xonsh-cat"
+    sed --in-place -e "1c$pythonShebang" "$out/bin/xonsh-uname"
+    sed --in-place -e "1c$pythonShebang" "$out/bin/xonsh-uptime"
+  '';
+
+  extraAttrs.meta.mainProgram = "xonsh";
+}))
