762b7b489cb592ed3501f58c13ec6d0129d8864e
Qyriad <qyriad@qyriad.me>
2025-09-02 13:17:35 +0200

    nixos: switch to en_GB locale

--- nixos/linux.nix
+++ nixos/linux.nix
@@ -101,7 +101,18 @@
 	#	#"sparc-linux"
 	#];
 
-	i18n.defaultLocale = "en_US.UTF-8";
+	i18n.defaultLocale = "en_GB.UTF-8";
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "en_US.UTF-8";
+		LC_IDENTIFICATION = "en_US.UTF-8";
+		LC_MEASUREMENT = "en_CA.UTF-8";
+		LC_MONETARY = "en_US.UTF-8";
+		LC_NAME = "en_US.UTF-8";
+		LC_NUMERIC = "en_US.UTF-8";
+		LC_PAPER = "en_US.UTF-8";
+		LC_TELEPHONE = "en_US.UTF-8";
+		LC_TIME = "en_GB.UTF-8";
+	};
 
 	# Add ~/.local/bin to system path.
 	environment.localBinInPath = true;
