8f9c5d071c604f51b61b577084c01360d0d61285
Qyriad <qyriad@qyriad.me>
2025-10-14 16:26:30 +0200

    nixos: move flake-module things into explicit config part of module

--- nixos/modules/flake-module.nix
+++ nixos/modules/flake-module.nix
@@ -12,33 +12,35 @@ let
 	bold = "${csi}[1m";
 in
 {
-	nixpkgs.overlays = [
-		flake.overlays.default
-	];
+	config = {
+		nixpkgs.overlays = [
+			flake.overlays.default
+		];
 
-	# Prevent our flake input trees from being garbage collected.
-	storePathsToKeep = flake.inputs;
+		# Prevent our flake input trees from being garbage collected.
+		storePathsToKeep = flake.inputs;
 
-	# Point "qyriad" to this here flake.
-	nix.registry.qyriad = {
-		from = { id = "qyriad"; type = "indirect"; };
-		flake = flake;
+		# Point "qyriad" to this here flake.
+		nix.registry.qyriad = {
+			from = { id = "qyriad"; type = "indirect"; };
+			flake = flake;
+		};
+
+		# Experimental: literally just add *all* our flake inputs as <nixPath> things.
+		nix.nixPath = lib.removeAttrs flake.inputs [ "nixpkgs" ]
+		|> lib.attrsToList
+		|> lib.map ({ name, value }: "${name}=${value}")
+		|> lib.trivial.concat [ "nixpkgs=flake:nixpkgs" ];
+
+		nixpkgs.flake.source = flake.inputs.nixpkgs.outPath;
+
+		# And for fun, let NixOS know our Git commit hash, if we have one.
+		system.configurationRevision = let
+			rev = flake.rev or flake.dirtyRev or "unknown";
+			sysName = config.system.name or config.system.darwinLabel;
+			msg = lib.trim ''
+				${green}note${normal}: '${sysName}' system configuration revision ${bold}${rev}${normal}
+			'';
+		in builtins.trace msg rev;
 	};
-
-	# Experimental: literally just add *all* our flake inputs as <nixPath> things.
-	nix.nixPath = lib.removeAttrs flake.inputs [ "nixpkgs" ]
-	|> lib.attrsToList
-	|> lib.map ({ name, value }: "${name}=${value}")
-	|> lib.trivial.concat [ "nixpkgs=flake:nixpkgs" ];
-
-	nixpkgs.flake.source = flake.inputs.nixpkgs.outPath;
-
-	# And for fun, let NixOS know our Git commit hash, if we have one.
-	system.configurationRevision = let
-		rev = flake.rev or flake.dirtyRev or "unknown";
-		sysName = config.system.name or config.system.darwinLabel;
-		msg = lib.trim ''
-			${green}note${normal}: '${sysName}' system configuration revision ${bold}${rev}${normal}
-		'';
-	in builtins.trace msg rev;
 }
