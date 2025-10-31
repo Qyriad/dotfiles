4450791b0ad0a5a856195cefc9e12afb1c22fb93
Qyriad <qyriad@qyriad.me>
2025-10-31 11:15:02 +0100

    nixos: yuki: disable networkd wait-online but enable networkd in general

--- nixos/yuki/default.nix
+++ nixos/yuki/default.nix
@@ -101,6 +101,22 @@
 		"dev"
 	];
 
+	systemd.network = {
+		enable = true;
+		# We're using NetworkManager.
+		wait-online.enable = false;
+		#netdevs."30-virt" = {
+		#	netdevConfig.Name = "br13";
+		#	netdevConfig.Kind = "bridge";
+		#};
+		#networks."30-virt" = {
+		#	matchConfig.Name = "br13";
+		#	networkConfig = {
+		#		DHCP = "yes";
+		#	};
+		#};
+	};
+
 	services.freshrss = {
 	};
 
