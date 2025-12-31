bf1058f5bd1ecf694f184aea6aa24ce988d25fef
Qyriad <qyriad@qyriad.me>
2025-12-31 15:12:15 +0100

    nvim: lsp: add type annotations in tsgo.lua

--- nvim/lsp/tsgo.lua
+++ nvim/lsp/tsgo.lua
@@ -1,6 +1,17 @@
 local qyriad = require('qyriad')
 
+qyriad.lsp = qyriad.lsp or { }
+qyriad.lsp.tsgo = qyriad.lsp.tsgo or { }
+---@param dispatchers vim.lsp.rpc.Dispatchers
+---@param config vim.lsp.ClientConfig
+---@return vim.lsp.rpc.PublicClient
+function qyriad.lsp.tsgo.cmd(dispatchers, config)
+end
+
 return qyriad.nested_tbl {
+	---@param dispatchers vim.lsp.rpc.Dispatchers
+	---@param config vim.lsp.ClientConfig
+	---@return vim.lsp.rpc.PublicClient
 	cmd = function(dispatchers, config)
 		local cmd = 'tsgo'
 		local local_cmd = (config or {}).root_dir and config.root_dir .. '/node_modules/.bin/tsgo'
