0d5fb2feda49649a104ef482482be1281246c373
Qyriad <qyriad@qyriad.me>
2026-01-08 14:37:24 +0100

    nixos: improve llvm-keg

--- nixos/pkgs/llvm-keg.nix
+++ nixos/pkgs/llvm-keg.nix
@@ -15,9 +15,17 @@
 		bintools = llvmPackages.bintools;
 	};
 
+	libgcc = libgcc.out;
+
+	llvm = llvmPackages.llvm.out;
+
 	meta.description = "llvm tools not in /bin";
 } <| lib.dedent ''
 	mkdir -p "$out/opt/llvm"
 	lndir -silent "$clang" "$out/opt/llvm"
 	lndir -silent "$lld" "$out/opt/llvm"
+	lndir -silent "$llvm" "$out/opt/llvm"
+	mkdir -p "$out/opt/llvm/include"
+	# Bleh. libstdc++ includes.
+	lndir -silent "$libgcc/include" "$out/opt/llvm/include"
 ''
