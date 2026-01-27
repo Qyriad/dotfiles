5026a6a3160048883cc8183ba5ad269b23a9d657
Qyriad <qyriad@qyriad.me>
2026-01-27 10:53:32 +0100

    nixos: maho: enable nixseparatedebuginfod

--- nixos/maho.nix
+++ nixos/maho.nix
@@ -49,6 +49,7 @@
 
 	programs.bash.completion.enable = true;
 	programs.nix-index.enable = true;
+	services.nixseparatedebuginfod2.enable = true;
 
 	programs.gnupg.agent = {
 		enable = lib.mkForce false;
