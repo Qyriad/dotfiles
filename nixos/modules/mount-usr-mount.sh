2430e8ed16c3ac127ff2e7752a021b93814c9a7d
Qyriad <qyriad@qyriad.me>
2026-01-28 10:39:11 +0100

    nixos: mount /usr read-only

--- nixos/modules/mount-usr-mount.sh
+++ nixos/modules/mount-usr-mount.sh
@@ -16,7 +16,7 @@ fi
 
 if [[ -h /run/current-system ]]; then
 	echo -n "99-mount-usr: "
-	"$MOUNT" "${ARGS[@]}" --verbose --bind /run/current-system/sw /usr || echo "oopsie! that didn't work!"
+	"$MOUNT" "${ARGS[@]}" --verbose --bind /run/current-system/sw /usr -o ro || echo "oopsie! that didn't work!"
 else
 	echo "99-mount-usr: /run/current-system is not mounted yet! Nothing to do!"
 fi
