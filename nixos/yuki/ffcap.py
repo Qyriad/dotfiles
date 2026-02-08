62fb341ac141099fab9024b15226b80acb0aa9e0
Qyriad <qyriad@qyriad.me>
2026-02-08 12:02:14 +0100

    nixos/ffcap: do logging to things other than stdout

--- nixos/yuki/ffcap.py
+++ nixos/yuki/ffcap.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+from datetime import datetime
 import copy
 from pathlib import Path
 from fcntl import fcntl, F_SETFL, F_GETFL
@@ -14,6 +15,7 @@ import subprocess
 import sys
 import typing
 from typing import Any
+from zoneinfo import ZoneInfo
 import time
 
 # ffmpeg -progress 'pipe:1' -nostdin -f video4linux2 -framerate 60 -video_size 1920x1080 -input_format yuyv422 -fflags nobuffer -i /dev/v4l/by-id/usb-Elgato_Game_Capture_HD60_S+_0004C809C2000-video-index0 -vf format=yuyv422 -f video4linux2 /dev/video10
@@ -25,27 +27,132 @@ def resolve_cmd(command: str) -> str:
 
     raise FileNotFoundError(f"no {command} found in PATH, nor overriden with ${command.upper()}")
 
+_LOG_PATH: Path | None = None
+_LOG_FILE: io.TextIOBase | None = None
+def log_file() -> io.TextIOBase:
+    global _LOG_PATH
+    global _LOG_FILE
+    if _LOG_FILE:
+        return _LOG_FILE
+
+    if not _LOG_PATH:
+        if from_systemd := os.environ.get('LOGS_DIRECTORY', None):
+            log_dir = Path(from_systemd)
+        elif xdg := os.environ.get('XDG_STATE_HOME'):
+            log_dir = Path(xdg) / 'log'
+        else:
+            log_dir = Path('/var/log')
+
+        try:
+            log_dir.mkdir(parents=True, exist_ok=True)
+        except OSError as e:
+            print(f"couldn't mkdir log directory: {e}")
+        _LOG_PATH = log_dir / "ffcap.log"
+
+    _LOG_FILE = open(_LOG_PATH, "a")
+
+    return _LOG_FILE
+
+_seen_exceptions = set()
+def write_log(*args, **kwargs):
+    global _seen_exceptions
+    try:
+        kwargs['file'] = log_file()
+        kwargs['flush'] = True
+        print(*args, **kwargs)
+    except Exception as e:
+        msg = str(e)
+        if msg not in _seen_exceptions:
+            _seen_exceptions.add(msg)
+            print(f"(Ignored): Error writing to log file: {e}", file=sys.stderr, flush=True)
+
+
 NOTIFY_ENABLED = "NOTIFY_SOCKET" in os.environ
 HAS_NOTIFIED = False
 
 DEBUG_PREFIX = '<7>: ' if NOTIFY_ENABLED else ''
 
-def proxy_to_stdout(file):
+class FpsBuffer(list[float]):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._every10 = 0
+
+    def _push(self, fps: float):
+        self.insert(0, fps)
+        if len(fps_buffer) > 10:
+            self._every10 += 1
+            self.pop()
+
+        if self._every10 >= 100:
+            write_log(f"FPS AVG: {self._avg()}")
+            self._every10 = 0
+
+    def _avg(self) -> float | None:
+        if len(self) < 10:
+            return None
+
+        return sum(self) / float(len(self))
+
+fps_buffer: FpsBuffer = FpsBuffer()
+
+def _add_fps(fps: float):
+    fps_buffer._push(fps)
+
+def _avg_too_low(threshold: float = 45.0) -> bool:
+    avg = fps_buffer._avg()
+    return avg is not None and avg < threshold
+
+    #if len(fps_buffer) < 10:
+    #    return False
+    #
+    #avg = sum(fps_buffer) / float(len(fps_buffer))
+    #return avg < threshold
+
+def _stop_if_fps_too_low(stdout: bytes, threshold: float = 30.0):
+    lines = stdout.decode('utf-8', errors='backslashreplace').splitlines()
+    try:
+        line = next(filter(lambda line: line.startswith('fps='), lines))
+    except StopIteration:
+        line = 'fps=0.0'
+
+    try:
+        (_, fps) = line.split('=')
+    except ValueError:
+        return
+
+    try:
+        fps = float(fps)
+    except ValueError:
+        return
+
+    if fps < threshold * 1.1:
+        print(f"<5>: FPS is low: {fps=}", flush=True)
+        write_log(f"FPS is low: {fps=}")
+
+    _add_fps(fps)
+    #print(f"{fps_buffer=}", flush=True)
+    if _avg_too_low(threshold):
+        raise TimeoutError(f"data rate got too low ({fps=})")
+
+def proxy_to_stdout(file: io.FileIO):
     global HAS_NOTIFIED
     data = file.read()
 
     if NOTIFY_ENABLED and not HAS_NOTIFIED and b'progress=continue' in data:
-        print("startup!")
+        print("startup!", flush=True)
         subprocess.check_call(["systemd-notify", "--ready"], executable=resolve_cmd("systemd-notify"))
         HAS_NOTIFIED = True
+
+    _stop_if_fps_too_low(data)
 
     if not data:
         raise TimeoutError("no stdout received in 4 seconds")
 
-def proxy_to_stderr(file):
+def proxy_to_stderr(file: io.FileIO):
     data = file.read()
-    #for line in data.decode('utf-8').splitlines():
-    #    print(f'{DEBUG_PREFIX}{line}')
+    for line in data.decode('utf-8').splitlines():
+        pass
+        #print(f'{DEBUG_PREFIX}{line}')
 
 
 def mainloop(in_systemd: bool, selector: selectors.BaseSelector):
@@ -78,13 +185,13 @@ def autodetect_capturecard() -> Path:
         results.append(json.loads(line))
 
     if len(results) > 1:
-        print("<2> multiple autodetect results found; using first one")
+        print("<2> multiple autodetect results found; using first one", flush=True)
 
     found = results[0]
     devlinks = shlex.split(found.get("DEVLINKS", ""))
     preferred = next(link for link in devlinks if "by-id" in link)
 
-    print(f"<3> preferred found: {preferred}")
+    print(f"<3> preferred found: {preferred}", flush=True)
 
     devnode = Path(preferred)
     if not devnode.exists():
@@ -175,7 +282,7 @@ class TristateOptionalAction(argparse.Action):
         ]
 
     def __call__(self, parser: argparse.ArgumentParser, namespace: argparse.Namespace, values, option_string=None):
-        print(f"{values=}\n{option_string=}")
+        print(f"{values=}\n{option_string=}", flush=True)
         if option_string not in self.option_strings:
             return
         assert option_string is not None
@@ -251,10 +358,10 @@ def main():
         outnode,
     ]
 
-    print(ffargs)
+    print(ffargs, flush=True)
 
     proc = subprocess.Popen(ffargs, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=0, executable=resolve_cmd("ffmpeg"))
-    print(f"{proc=}")
+    print(f"{proc=}", flush=True)
     assert proc.stdout is not None
     assert proc.stderr is not None
 
@@ -276,16 +383,16 @@ def main():
                 key.data(key.fileobj)
 
         except KeyboardInterrupt:
-            print("Killed by CtrlC")
+            print("Killed by CtrlC", flush=True)
             proc.kill()
             raise
 
         except subprocess.TimeoutExpired as e:
-            print(f"Timed out: {e}")
+            print(f"Timed out: {e}", flush=True)
             proc.kill()
             raise
         except Exception as e:
-            print(f"Other error {e}")
+            print(f"Other error {e}", flush=True)
             proc.kill()
             raise
 
