4f5daf66b7f9cb66dd0c81c3b49ddc27e4ca6dfe
Qyriad <qyriad@qyriad.me>
2026-02-12 11:31:56 +0100

    nixos: disable lix substituter
    literally when has it ever come up

--- nixos/common.nix
+++ nixos/common.nix
@@ -46,9 +46,9 @@
 				"auto-allocate-uids"
 			];
 
-			extra-substituters = [
-				"https://cache.lix.systems"
-			];
+			#extra-substituters = [
+			#	"https://cache.lix.systems"
+			#];
 
 			trusted-public-keys = [
 				"cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
