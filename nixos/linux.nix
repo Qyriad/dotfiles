4460f2ee80c1e2a45df28bfd0d3d8805e018b118
Qyriad <qyriad@qyriad.me>
2026-03-10 16:36:30 +0100

    nixos: systemd-mutable is already handled by extra-containers

--- nixos/linux.nix
+++ nixos/linux.nix
@@ -9,8 +9,6 @@
 		efi.efiSysMountPoint = "/boot/efi";
 	};
 
-	boot.extraSystemdUnitPaths = [ "/etc/systemd-mutable/system" ];
-
 	# We want sysrqs to work.
 	#boot.kernelParams = [ "sysrq_always_enabled" ];
 	boot.kernel.sysctl = {
