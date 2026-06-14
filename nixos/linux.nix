ff4d062d7a69b77e232e63a9edab1a13d10b6f60
Qyriad <qyriad@qyriad.me>
2026-06-14 13:32:22 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'lix':
        'git+https://git.lix.systems/lix-project/lix?ref=refs/heads/main&rev=ab149a6cd8aef3f413c174e1763d0c7e8cd37970' (2026-06-09)
      → 'git+https://git.lix.systems/lix-project/lix?ref=refs/heads/main&rev=12baca7bea94cc1b54f0ffe0d458a66a243edf99' (2026-06-13)
    • Updated input 'nix-darwin':
        'github:nix-darwin/nix-darwin/6a771120d607dcccb279a27d227650e324815c35' (2026-06-07)
      → 'github:nix-darwin/nix-darwin/aabb2037edfc0f210723b72cd5f528aab5dd3f0b' (2026-06-12)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/a799d3e3886da994fa307f817a6bc705ae538eeb' (2026-06-06)
      → 'github:NixOS/nixpkgs/9ae611a455b90cf061d8f332b977e387bda8e1ca' (2026-06-10)
    • Updated input 'xonsh-source':
        'github:xonsh/xonsh/7b0d1a8419f219ea7b89e751baf7b4ab63232882' (2026-06-09)
      → 'github:xonsh/xonsh/d18710d7323de1739afa2e6a54207cb3b274ef5c' (2026-06-14)

--- nixos/linux.nix
+++ nixos/linux.nix
@@ -49,9 +49,9 @@
 		StatusUnitFormat = "combined";
 	};
 
-	systemd.user.extraConfig = lib.dedent ''
-		StatusUnitFormat=combined
-	'';
+	systemd.user.settings.Manager = {
+		StatusUnitFormat = "combined";
+	};
 
 	systemd.slices.system-builder.sliceConfig = config.resources.builderSliceConfig;
 	systemd.user.slices.user-builder.sliceConfig = config.resources.builderSliceConfig;
