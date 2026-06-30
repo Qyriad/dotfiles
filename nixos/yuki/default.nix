718a280e571430ace4d9153fcb2a97a1defd42d5
Qyriad <qyriad@qyriad.me>
2026-06-30 14:18:56 +0200

    nixos: fix vesktop build

--- nixos/yuki/default.nix
+++ nixos/yuki/default.nix
@@ -26,6 +26,7 @@
 	nixpkgs.config.permittedInsecurePackages = [
 		"ventoy-qt5-1.1.07"
 		"olm-3.2.16"
+		"pnpm-10.29.2" # For Vesktop.
 	];
 	nixpkgs.overlays = let
 		kwin-useractions-no-overflow = pkgsFinal: pkgsPrev: {
