d510dec52b000d2a1168d4b84fa86fbe215f8b05
Qyriad <qyriad@qyriad.me>
2026-07-08 14:23:46 +0200

    nixos/lumar: stage3

--- nixos/lumar.nix
+++ nixos/lumar.nix
@@ -1,14 +1,16 @@
 # vim: shiftwidth=0 tabstop=2 noexpandtab
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 
 {
 	imports = [
 		./common.nix
+		./dev.nix
 		# FIXME: this import should probably be moved to a generic darwin.nix or something.
 		./modules/darwin-nixos-compat.nix
 	];
 
 	system.stateVersion = 7;
+	nixpkgs.hostPlatform = lib.mkDefault { system = "aarch64-darwin"; };
 
 	nixpkgs.config.allowUnfree = true;
 
@@ -44,11 +46,14 @@
 		};
 	};
 
+	# Some hardware stuffs.
 
 	system.keyboard = {
 		enableKeyMapping = true;
 		remapCapsLockToEscape = true;
 	};
+
+	networking.wakeOnLan.enable = true;
 
 	fonts.packages = with pkgs; [
 		nerd-fonts.inconsolata-go
@@ -63,14 +68,29 @@
 	};
 	programs.tmux.enable = true;
 
+	# GUI stuff in programs.*
+	programs._1password-gui.enable = true;
+
 	homebrew = {
 		enable = true;
 		casks = [
-		];
-		greedyCasks = [
 			# Karabiner Elements' extensions and services are a little awkward from nix-darwin.
 			{ name = "karabiner-elements"; greedy = true; }
+
+			{ name = "beeper"; greedy = true; }
+			{ name = "signal@beta"; greedy = true; }
 		];
+		masApps = {
+			# Safari Extensions.
+			"1Password for Safari" = 1569813296;
+			"Userscripts" = 1463298887;
+			"Kagi for Safari" = 1622835804;
+			"1Blocker: Ad Blocker" = 1365531024;
+			"Noir" = 1592917505;
+			"Refined GitHub" = 1519867270;
+			"Consent-O-Matic" = 1606897889;
+			"Amphetamine" = 937984704;
+		};
 	};
 
 	environment.systemPackages = with pkgs; [
@@ -104,10 +124,13 @@
 
 		# Core apps.
 		wezterm
-		_1password-gui
+		config.programs._1password-gui.package
 
 		# Essential macOS addition.
 		rectangle
 		thaw
+
+		# General GUI stuff.
+		vesktop
 	];
 }
